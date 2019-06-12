f71430600576ea36ae4364b528d30ac3546156be
zhanghaozhi <623920249@qq.com>
2019-06-12 20:00:28 +0800

    把本地工程添加到git

---
+++ src/com/itheima/git/DemoGit.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class DemoGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
